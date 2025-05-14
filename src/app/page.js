ac9e1f7e7e5e773c410b8b670697c68635bd0be9
Damon Pitkethley <damonpitkethley53@gmail.com>
2025-05-14 23:28:29 +1200

    make app mobile friendly

--- src/app/page.js
+++ src/app/page.js
@@ -53,24 +53,24 @@ export default function Home() {
   }, [IncomeList, ExpensesList]);
 
   return (
-    <div className="flex flex-col gap-4 m-auto w-1/3 p-4 bg-slate-200 rounded-lg">
+    <div className="flex flex-col gap-4 m-auto w-full md:w-2/3 lg:w-1/2 h-full p-2 bg-slate-200 rounded-lg">
       <div className="flex gap-1 w-full bg-slate-100">
-        <div className="w-full h-full bg-white min-h-96 text-center">
+        <div className="w-full h-full bg-white rounded-lg p-2 min-h-96 text-center">
           {ExpensesList.map((expense, index) => (
             <div
               key={`expense_${index}`}
-              className="text-red-500 text-xl font-medium hover:line-through hover:cursor-pointer"
+              className="text-red-500 md:text-lg font-medium hover:line-through hover:cursor-pointer"
               onClick={() => DeleteExpense(index)}
             >
               -${expense.amount} | {expense.desc}
             </div>
           ))}
         </div>
-        <div className="w-full h-full bg-white min-h-96 text-center">
+        <div className="w-full h-full bg-white rounded-lg p-2 min-h-96 text-center">
           {IncomeList.map((income, index) => (
             <div
               key={`income_${index}`}
-              className="text-green-500 text-xl font-medium hover:line-through hover:cursor-pointer"
+              className="text-green-500 md:text-lg font-medium hover:line-through hover:cursor-pointer"
               onClick={() => DeleteIncome(index)}
             >
               +${income.amount} | {income.desc}
@@ -79,11 +79,11 @@ export default function Home() {
         </div>
       </div>
 
-      <h1 className="text-black text-center text-3xl border-b-2 border-black pb-4">
+      <h1 className="text-black text-center text-2xl border-b-2 border-black pb-4">
         Balance: ${RemainingBalance}
       </h1>
 
-      <div className="flex gap-2 w-full justify-center items-center">
+      <div className="flex flex-col md:flex-row gap-2 w-full justify-center items-center">
         <div className="flex flex-col">
           <label className="text-black text-sm text-center" htmlFor="desc">
             Description
@@ -111,7 +111,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex gap-2 justify-center">
+      <div className="flex gap-2 justify-center items-center">
         <button
           className="text-lg text-red-900 p-2 w-30 bg-red-400 rounded hover:cursor-pointer hover:text-white"
           onClick={AddExpense}
